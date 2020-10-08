a748569d2ed1be872afede82cd20f745623f039b
suraj27499 <surajkumardubey27499@gmail.com>
2020-10-08 19:25:55 +0530

    create account

--- app/src/main/java/com/urk17cs290/banker/activities/CreateAccount.java
+++ app/src/main/java/com/urk17cs290/banker/activities/CreateAccount.java
@@ -2,15 +2,72 @@ package com.urk17cs290.banker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.urk17cs290.banker.MainActivity;
 import com.urk17cs290.banker.R;
 
 public class CreateAccount extends AppCompatActivity {
+    Button createAccountButton;
+    EditText name,email,password,mobile;
+    Spinner accounttype;
+    Intent myintent;
+    String ac_type_value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        name = findViewById(R.id.name);
+        email = findViewById( R.id.email);
+        password = findViewById(R.id.password);
+        mobile  = findViewById(R.id.phone);
+        accounttype = findViewById(R.id.accounttype);
+        createAccountButton = findViewById(R.id.createAccount);
+
+        accounttype.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ac_type_value = ((TextView)view).getText().toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                accounttype.setSelection(0);
+                ac_type_value = "Savings Account";
+
+            }
+        });
+
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name1 = name.getText().toString();
+                String email1 = email.getText().toString();
+                String password1 = password.getText().toString();
+                String mobile1  = mobile.getText().toString();
+                String accounttype1 =ac_type_value;
+
+                /*todo
+                save values to database
+                 */
+                myintent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(myintent);
+
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/urk17cs290/banker/activities/SplashScreen.java
+++ app/src/main/java/com/urk17cs290/banker/activities/SplashScreen.java
@@ -7,31 +7,37 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.urk17cs290.banker.MainActivity;
 import com.urk17cs290.banker.R;
 
 public class SplashScreen extends AppCompatActivity {
 Intent myintent1,myintent2;
 Button b1,b2;
+public static String user_or_admin= "";
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash_screen);
-   /* myintent1 = new Intent(getApplicationContext(),xyz.class);//chnge
-    myintent2 = new Intent(getApplicationContext(),xyz.class);//change
+    myintent1 = new Intent(getApplicationContext(), MainActivity.class);
+    b1 = findViewById(R.id.containedButton1);
+    b2 = findViewById(R.id.containedButton2);
+
     b1.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        startActivity();//change
+        user_or_admin="admin";
+        startActivity(myintent1);//change
       }
     });
     b2.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        startActivity();//change
+        user_or_admin="user";
+        startActivity(myintent1);//change
       }
     });
 
-    */
+
 
   }
 }
