a7d108972882821c335d02e13768933546643dcd
suraj27499 <surajkumardubey27499@gmail.com>
2020-10-08 22:32:10 +0530

    debit account ui

--- app/src/main/java/com/urk17cs290/banker/activities/DebitAccount.java
+++ app/src/main/java/com/urk17cs290/banker/activities/DebitAccount.java
@@ -1,16 +1,36 @@
 package com.urk17cs290.banker.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.urk17cs290.banker.MainActivity;
 import com.urk17cs290.banker.R;
 
 public class DebitAccount extends AppCompatActivity {
-
+  Button b ;
+  EditText input;
+  Intent myintent;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_debit_account);
+    b = findViewById(R.id.debitButton);
+    input = findViewById(R.id.debit);
+    myintent = new Intent(getApplicationContext(), MainActivity.class);
+    b.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        /*TODO
+         * diplay a toast debited successfully
+         * update to database
+         * */
+        startActivity(myintent);
+      }
+    });
   }
 }
