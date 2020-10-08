faa836edd47ec07a85963514d9714d8428a73e95
suraj27499 <surajkumardubey27499@gmail.com>
2020-10-08 17:09:03 +0530

    get balance

--- app/src/main/java/com/urk17cs290/banker/activities/SplashScreen.java
+++ app/src/main/java/com/urk17cs290/banker/activities/SplashScreen.java
@@ -2,17 +2,36 @@ package com.urk17cs290.banker.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.urk17cs290.banker.R;
 
 public class SplashScreen extends AppCompatActivity {
-Intent myintent;
+Intent myintent1,myintent2;
+Button b1,b2;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash_screen);
-    myintent = new Intent(getApplicationContext(),display.class);
+   /* myintent1 = new Intent(getApplicationContext(),xyz.class);//chnge
+    myintent2 = new Intent(getApplicationContext(),xyz.class);//change
+    b1.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        startActivity();//change
+      }
+    });
+    b2.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        startActivity();//change
+      }
+    });
+
+    */
+
   }
 }
