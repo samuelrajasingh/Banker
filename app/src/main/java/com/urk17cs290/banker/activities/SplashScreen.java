9c3d67097189892e7c8d9b01876960e3aa09f37f
suraj27499 <surajkumardubey27499@gmail.com>
2020-10-08 15:35:25 +0530

    splashscreen

--- app/src/main/java/com/urk17cs290/banker/activities/SplashScreen.java
+++ app/src/main/java/com/urk17cs290/banker/activities/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.urk17cs290.banker.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,10 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.urk17cs290.banker.R;
 
 public class SplashScreen extends AppCompatActivity {
-
+Intent myintent;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash_screen);
+    myintent = new Intent(getApplicationContext(),display.class);
   }
 }
