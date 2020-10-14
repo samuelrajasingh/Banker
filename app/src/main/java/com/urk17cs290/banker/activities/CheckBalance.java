1a69859b4960ca2793193bbfb39f17792872d5de
suraj27499 <surajkumardubey27499@gmail.com>
2020-10-14 21:13:05 +0530

    shared prefs

--- app/src/main/java/com/urk17cs290/banker/activities/CheckBalance.java
+++ app/src/main/java/com/urk17cs290/banker/activities/CheckBalance.java
@@ -4,10 +4,13 @@
 
 package com.urk17cs290.banker.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -20,29 +23,43 @@ import com.urk17cs290.banker.viewmodels.AccountViewModel;
 public class CheckBalance extends AppCompatActivity {
     TextView setBalance;
     Button checkBalance;
-    TextInputLayout ac, pass;
+    SharedPreferences myprefs;
+    SharedPreferences.Editor editor;
+    int password;
+    Boolean isLoggedin;
+    int accountNumber;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_balance);
         setBalance = findViewById(R.id.showBalance); // textview
-        ac = findViewById(R.id.enter_acnumber);//ac number
-        pass = findViewById(R.id.enter_pin);
+
         checkBalance = findViewById(R.id.check_balance_button); // button
+        isLoggedin = myprefs.getBoolean("isLoggedin",false);
+        accountNumber = myprefs.getInt("accountNumber",000000);
+        password = myprefs.getInt("password",000);
+        intent = new Intent(getApplicationContext(),Login.class);
         checkBalance.setOnClickListener(view -> {
             /*TODO
              * get balance
              * set as a string
              * */
-            int accountNumber;
-            accountNumber = Integer.parseInt(String.valueOf(ac.getEditText().getText()));
-            int pin;
-            pin = Integer.parseInt(String.valueOf(pass.getEditText().getText()));
+            if(isLoggedin){
+                /*TODO
+                 * diplay a toast debited successfully
+                 * update to database
+                 * */
 
-            AccountViewModel accountViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(AccountViewModel.class);
-            Account account = accountViewModel.search(accountNumber);
-            Log.e("TAG", "onCreate: accounts " + account);
+
+            }else{
+                Toast.makeText(this, "Login again", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+
+
         });
     }
 }
